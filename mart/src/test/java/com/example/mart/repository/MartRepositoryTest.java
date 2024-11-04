c49b69786c5a2b86b86aebf8202ae058cca609e1
김현진 <summersad10@gmail.com>
2024-11-04 18:18:12 +0900

    JPA 테이블 연습 Mart

---
+++ mart/src/test/java/com/example/mart/repository/MartRepositoryTest.java
@@ -0,0 +1,183 @@
+package com.example.mart.repository;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.mart.entity.constant.OrderStatus;
+import com.example.mart.entity.item.Item;
+import com.example.mart.entity.item.Member;
+import com.example.mart.entity.item.Order;
+import com.example.mart.entity.item.OrderItem;
+import com.example.mart.repository.item.ItemRepository;
+import com.example.mart.repository.item.MemberRepository;
+import com.example.mart.repository.item.OrderItemRepository;
+import com.example.mart.repository.item.OrderRepository;
+
+// 객체 그래프 탐색
+
+@SpringBootTest
+public class MartRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    // C
+    @Test
+    public void memberInsertTest() {
+        memberRepository.save(Member.builder().name("user1").city("서울시").street("123-45").zipcode("15100").build());
+        memberRepository.save(Member.builder().name("user2").city("경기도").street("123-46").zipcode("16100").build());
+        memberRepository.save(Member.builder().name("user3").city("인천시").street("123-47").zipcode("17100").build());
+
+    }
+
+    @Test
+    public void itemInsertTest() {
+        itemRepository.save(Item.builder().name("tshirt").price(40000).quantity(10).build());
+        itemRepository.save(Item.builder().name("skirt").price(50000).quantity(20).build());
+        itemRepository.save(Item.builder().name("hood").price(60000).quantity(30).build());
+
+    }
+
+    @Test
+    public void orderInsertTest() {
+        Member member = memberRepository.findById(1L).get();
+        Item item = itemRepository.findById(1L).get();
+
+        Order order = Order.builder()
+                .orderDate(LocalDateTime.now())
+                .status(OrderStatus.ORDER)
+                .member(member)
+                .build();
+        orderRepository.save(order);
+
+        OrderItem orderItem = OrderItem.builder()
+                .price(200000)
+                .count(2)
+                .order(order)
+                .item(item)
+                .build();
+        orderItemRepository.save(orderItem);
+    }
+
+    // R
+    @Test
+    public void memberAndItemOrderListTest() {
+
+        // 주문내역 조회
+        // orderRepository.findAll().forEach(order -> System.out.println(order));
+
+        // 주문 상세 내역
+        // orderRepository.findAll().forEach(order -> {
+        // System.out.println(order);
+        // System.out.println(order.getMember());
+        // });
+
+        orderItemRepository.findAll().forEach(orderItem -> {
+
+            System.out.println(orderItem);
+            // 상품 상세 조회
+            System.out.println(orderItem.getItem());
+            // 주문 상세 내역 조회
+            System.out.println(orderItem.getOrder());
+            // 주문자 상세 조회
+            System.out.println(orderItem.getOrder().getMember());
+        });
+    }
+
+    @Test
+    public void memberAndItemAndOrderRowTest() {
+
+        // 주문 하나 조회
+        OrderItem orderItem = orderItemRepository.findById(1L).get();
+        System.out.println(orderItem);
+        // 상품 상세 조회
+        System.out.println(orderItem.getItem());
+        // 주문 상세 내역 조회
+        System.out.println(orderItem.getOrder());
+        // 주문자 상세 조회
+        System.out.println(orderItem.getOrder().getMember());
+
+    }
+
+    // U
+    @Test
+    public void memberAndItemAndOrderUpdateTest() {
+
+        // 주문자의 주소 변경
+        // Member member = Member.builder()
+        // .id(1L)
+        // .city("서울시")
+        // .name("user1")
+        // .street("255-10")
+        // .zipcode("15100")
+        // .build();
+        Member member = memberRepository.findById(1L).get();
+        member.setStreet("432-10");
+
+        // save : insert or update
+        // 엔티티 매니저가 있어서 현재 entity 가 new 인지 기존 entity 인지 구분이 가능함
+        // new : insert 호출 / 기존 entity : update 호출
+        // update 는 무조건 전체 컬럼이 수정되는 형태로 작성됨
+        System.out.println(memberRepository.save(member));
+
+        // 1번 주문 상품의 가격 변경
+        // 아이템 수량, 가격 변경
+        Item item = itemRepository.findById(1L).get();
+        item.setPrice(102000);
+        itemRepository.save(item);
+
+        // 연결된 테이블 모두 수정
+        OrderItem orderItem = orderItemRepository.findById(1L).get();
+        orderItem.setPrice(102000);
+        orderItemRepository.save(orderItem);
+
+    }
+
+    // D
+    @Test
+    public void memberAndItemAndOrderDeleteTest() {
+        // 주문 취소
+        // 무결성 제약조건(C##JPAUSER.FK5ODJTCYIRCY7XU4QTOLJDHKRJ)이 위배되었습니다- 자식 레코드가 발견되었습니다
+        // 주문상품 취소
+        orderItemRepository.deleteById(1L);
+
+        // 주문 취소
+        orderRepository.deleteById(1L);
+    }
+
+    // 양방향
+    // Order ==> OrderItem 객체 그래프 탐색
+    @Transactional
+    @Test
+    public void testOrderItemList() {
+        Order order = orderRepository.findById(2L).get();
+        System.out.println(order);
+        // Order ==> OrderItem 탐색 시도
+        // org.hibernate.LazyInitializationException => 패치전략 바꾸기
+        order.getOrderItemsList().forEach(orderItem -> System.out.println(orderItem));
+    }
+
+    @Transactional
+    @Test
+    public void testOrderList() {
+        Member member = memberRepository.findById(1L).get();
+        System.out.println(member);
+
+        member.getOrders().forEach(order -> System.out.println(order));
+
+    }
+
+}
