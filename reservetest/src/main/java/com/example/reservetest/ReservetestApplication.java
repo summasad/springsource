5c7ef2477f86c18e5006cc0c4d7a6911118d8b9d
김현진 <summersad10@gmail.com>
2024-12-04 17:30:55 +0900

    test

---
+++ reservetest/src/main/java/com/example/reservetest/ReservetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservetestApplication.class, args);
+	}
+
+}
---
+++ reservetest/src/test/java/com/example/reservetest/ReservetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
